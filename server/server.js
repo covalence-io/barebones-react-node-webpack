c83897ac0e86dbe1f134dd34e88fcf2dfdb6b2b9
przekwas <luke@covalence.io>
2020-07-30 10:34:42 -0500

    updating packages and config

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ console.log(p);
 app.use(express.static(p));
 app.use(apiRouter);
 
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,53 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 let serverConfig = {
-    entry: './server/server.js',
-    output: {
-        path: path.join(__dirname, './dist/'),
-        filename: 'server.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    externals: [nodeExternals()],
-    target: 'node' ,
-    node: {
-        __dirname: false
-    }
-}
+	mode: process.env.NODE_ENV || 'development',
+	entry: './server/server.js',
+	output: {
+		path: path.join(__dirname, './dist/'),
+		filename: 'server.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	externals: [nodeExternals()],
+	target: 'node',
+	node: {
+		__dirname: false
+	}
+};
 
 let clientConfig = {
-    entry: './client/index.jsx',
-    output: {
-        path: path.join(__dirname, './public/js/'),
-        filename: 'app.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader','css-loader']
-            }
-        ]
-    }
-}
+	mode: process.env.NODE_ENV || 'development',
+	entry: './client/index.jsx',
+	output: {
+		path: path.join(__dirname, './public/js/'),
+		filename: 'app.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	}
+};
 
 module.exports = [serverConfig, clientConfig];
